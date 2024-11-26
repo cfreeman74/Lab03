7581f2c210096b0c8c107c0fdfc26fe578acbb17
cfreeman74 <shareesefreeman@gmail.com>
2024-11-26 22:10:06 +0000

    d

--- Home_Page.py
+++ Home_Page.py
@@ -24,8 +24,8 @@ Welcome to our Streamlit Web Development Lab03 app! You can navigate between the
 
 1. Netra's Profile: Online portfolio of Netra Somasundaram \n
 2. Jayce's Profile: Online portfolio for the 'Best' Councilor in Arcane\n
-3. Star Wars API: A Star Wars search tool that can find your Star Wars lookalike\n
-4. Gemini API: A Star Wars height competiton including an AI chatbot. 
+3. Star Wars Lookalike: A Star Wars search tool that can find your Star Wars lookalike\n
+4. Star Wars Height Competiton: A Star Wars height competiton including an AI chatbot. 
 
 """)
 
