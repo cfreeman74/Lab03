e5c0c974e4521df5f6981c0681a67245febaeb76
cfreeman74 <shareesefreeman@gmail.com>
2024-11-26 20:45:52 +0000

    x

--- pages/geminiAPI.py
+++ pages/geminiAPI.py
@@ -59,6 +59,19 @@ def storage(result):
     response = model.generate_content(prompt)
     st.write(response.text)
 
+def chatbot():
+    model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat(
+    history=[
+        {"role": "user", "parts": "Hello"},
+        {"role": "model", "parts": "Great to meet you. What would you like to know?"},
+    ]
+)
+response = chat.send_message("I have 2 dogs in my house.")
+print(response.text)
+response = chat.send_message("How many paws are in my house?")
+print(response.text)
+
 
 st.header("Who's taller?")
 st.write("Put in the characters of your chioice.")
