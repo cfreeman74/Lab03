b8d6a8098d0ca7b288886c317631c976bffbb64c
cfreeman74 <shareesefreeman@gmail.com>
2024-11-26 21:52:35 +0000

    s

--- pages/geminiAPI.py
+++ pages/geminiAPI.py
@@ -110,7 +110,7 @@ history=[
 )
 
 # Create the Streamlit app
-st.set_page_config(page_title="Star Wars Chatbot", layout="centered")
+#st.set_page_config(page_title="Star Wars Chatbot", layout="centered")
 
     # Page header
 st.markdown(
