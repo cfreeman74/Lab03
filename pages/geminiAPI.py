15a19d84d651026fde48dcec84ec029539b8847e
cfreeman74 <shareesefreeman@gmail.com>
2024-11-26 17:15:27 +0000

    ds

--- pages/geminiAPI.py
+++ pages/geminiAPI.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 key = st.secrets["key"]
 
-genai.configure(api_key=API_KEY)
+genai.configure(api_key=key)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a story about a magic backpack.")
