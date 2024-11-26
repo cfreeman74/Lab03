37fd3bc2d9efcc8eb3c77664875f43463b4a9ad5
cfreeman74 <shareesefreeman@gmail.com>
2024-11-26 16:17:15 +0000

    s

---
+++ pages/geminiAPI.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+import os
+import streamlit as st
+
+export API_KEY=<AIzaSyCIvvMoX4aMLXKVIkm9snaZ-GuQL2aR9zo>
+
+genai.configure(api_key=os.environ["API_KEY"])
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Write a story about a magic backpack.")
+print(response.text)
